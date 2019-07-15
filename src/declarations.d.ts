f4cbb4e1e8085c78a850c6d8413a7c9d41ff0bdf
Garrett Weems <gwgraphicdesign@gmail.com>
2019-07-15 15:56:45 -0400

    ➖ Removing crap

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-=======
-/* eslint-disable @typescript-eslint/no-explicit-any */
-// This file is used to hold ambient type declarations, as well as type shims
-// for npm module without type declarations, and assets files.
-
-// For example, to shim modules without declarations, use:
-// declare module 'package-without-declarations';
-
-// And to shim assets, use (one file extension per `declare`):
-// declare module '*.png';
-
->>>>>>> master
 interface Err extends Error {
   status: number;
   data?: Record<string, any>;
--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 /* eslint-disable @typescript-eslint/no-explicit-any */
-=======
->>>>>>> master
 /* eslint-disable no-unused-expressions */
 import { Request, Response, NextFunction, RequestHandler } from 'express';
 import * as Validator from 'validatorjs';
@@ -12,7 +9,6 @@ export const wrap = (fn: RequestHandler) => (
   next: NextFunction
 ) => fn(req, res, next).catch(next);
 
-<<<<<<< HEAD
 export function error({
   status,
   message,
@@ -22,16 +18,12 @@ export function error({
   message: string;
   data?: any;
 }): Error {
-=======
-export function error(status: number, message: string, data?: any): Error {
->>>>>>> master
   const err = new Error(message) as Err;
   err.status = status;
   err.data = data;
   return err;
 }
 
-<<<<<<< HEAD
 export const validate = ({
   data,
   rules,
@@ -64,36 +56,11 @@ export const validateBody = ({
   customErrorMessages?: Validator.ErrorMessages;
 }): RequestHandler => (req: Request, res: Response, next: NextFunction): void =>
   validate({ data: req.body, rules, next, customErrorMessages });
-=======
-export const validate = (
-  data: any,
-  rules: any,
-  next: NextFunction,
-  customErrorMessages?: Validator.ErrorMessages
-) => {
-  const validator = new Validator(data, rules, customErrorMessages);
-
-  return validator.fails()
-    ? next(error(400, 'Bad Request', validator.errors.all()))
-    : next();
-};
-
-export const validateBody = (
-  rules: any,
-  customErrorMessages?: Validator.ErrorMessages
-): RequestHandler => (req: Request, res: Response, next: NextFunction): void =>
-  validate(req.body, rules, next, customErrorMessages);
->>>>>>> master
 
 export const validateQuery = (
   rules: any,
   customErrorMessages?: Validator.ErrorMessages
-<<<<<<< HEAD
 ): RequestHandler => {
   return (req: Request, res: Response, next: NextFunction): void =>
     validate({ data: req.query, rules, next, customErrorMessages });
 };
-=======
-): RequestHandler => (req: Request, res: Response, next: NextFunction): void =>
-  validate(req.query, rules, next, customErrorMessages);
->>>>>>> master
