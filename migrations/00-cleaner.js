423bf772076d0edf3867f3e60493ca8f8a5ab6bf
Garrett Weems <gwgraphicdesign@gmail.com>
2019-07-22 14:50:30 -0400

    🐛 Fix cleaner migration

--- migrations/00-cleaner.js
+++ migrations/00-cleaner.js
@@ -1,11 +1,10 @@
-// import knexCleaner from 'knex-cleaner';
-const knexCleaner = require('knex-cleaner');
-
 exports.up = async knex => {
   await knex.schema.dropTableIfExists('flavors');
   await knex.schema.dropTableIfExists('menu');
   await knex.schema.dropTableIfExists('events');
 };
-
-exports.down = knex =>
-  knexCleaner.clean(knex, { mode: 'delete', restartIdentity: true });
+exports.down = async knex => {
+  await knex.schema.dropTableIfExists('flavors');
+  await knex.schema.dropTableIfExists('menu');
+  await knex.schema.dropTableIfExists('events');
+};
